196ccaa8b164226dbcdde71e71b60d7bfe87935a
PanJX <pjx8888@outlook.com>
2025-03-23 21:06:03 +0800

    fix: 更新后端 API 地址并移除明文流量设置
    - 将后端地址从 http 改为 https，提高数据传输安全性
    - 移除 AndroidManifest 中的 usesCleartextTraffic 设置，避免安全警告

--- app/src/main/java/com/panjx/clouddrive/core/config/Config.kt
+++ app/src/main/java/com/panjx/clouddrive/core/config/Config.kt
@@ -3,5 +3,6 @@ package com.panjx.clouddrive.core.config
 // 配置文件
 object Config {
     // 后端地址
-    const val ENDPOINT = "http://cn-qz-plc-1.ofalias.net:50318/"
+    const val ENDPOINT = "https://api.1216.ink/"
+    //const val ENDPOINT = "http://cn-qz-plc-1.ofalias.net:50318/"
 }
